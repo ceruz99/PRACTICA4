fa77b6adc558c2488fe7b5a7a790b3fcc5b6ed4e
Camilo Mazo <camilo.mazo2@udea.edu.co>
2021-08-24 18:55:30 -0500

    Se agrega la primera parte del menú.

--- practica4/main.cpp
+++ practica4/main.cpp
@@ -21,7 +21,6 @@ int main()
     int n=0;//numero de enrutadores
 
     ifstream leer;
-
     try {
         leer.open(DOC);
         if(!leer.is_open())
@@ -31,9 +30,7 @@ int main()
         if(c=='1')
             cout<<"Problema con la lectura del archivo de la red"<<endl;
     }
-
     string linea;
-
     while(getline(leer,linea)){
         string nombre;
         string costo;
@@ -44,17 +41,55 @@ int main()
         nombres.push_back(nombre);
         costos.push_back(atoi(costo.c_str()));
         n+=1;
-        cout<<"Se está leyendo"<<endl;
     }
     //While que saca los nombres y costos de los enrutadores para meterlos en los vectores.
     leer.close();
 
-
     enrutador red;
+    red.modificacion(&costos[0],&nombres[0],n);
+    red.calcularRutas();//Se calculan todas las mejores rutas de cada enrutador.
+
+    while(true){
+        int opcion=0;
+        cout<<"Menu"<<endl<<"Ingrese un numero para acceder a las opciones:"<<endl
+        <<"1.Agregar un nuevo enrutador."<<endl
+        <<"2.Eliminar un enrutador"<<endl
+        <<"3.Modificar costo de un enrutador a otro"<<endl
+        <<"4.Conocer el costo de un enrutador a otro"<<endl;
+        system("CLS");
+        switch (opcion) {
+        case 1:{
+            string nombre;
+            int costo=0;
+            cout<<"Ingrese el nombre del enrutador: ";cin>>nombre;
+            system("CLS");
+            cout<<endl<<"El enrutador "<<nombre<<" se conectara a "<<nombres[n-1]<<" y a "
+            <<nombres[0]<<endl;
+            cout<<"Ingrese el costo que habra de  "<<nombre<<" a "<<nombres[n-1];cin>>costo;
+
+            nombres.push_back(nombre);
+            costos.push_back(costo);
+            n+=1;
+            red.modificacion(&costos[0],&nombres[0],n);
+            red.calcularRutas();
+
+            break;
+        }
+        }
+    }
+
+
+
+    cout<<"probando.."<<endl;
+
+    nombres.erase(nombres.begin()+1);
+    costos.erase(costos.begin()+1);
+    n-=1;
     red.modificacion(&costos[0],&nombres[0],n);
     red.calcularRutas();
 
     cout<<"probando.."<<endl;
+
     return 0;
 }
 
